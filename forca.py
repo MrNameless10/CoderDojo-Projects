5c4145054eb38a8b26abfeb3a39e688a0d2f1975
MrNameless10 <61701003+MrNameless10@users.noreply.github.com>
2022-03-08 21:35:06 +0000

    Comment for better explaining to kids

--- forca.py
+++ forca.py
@@ -1,4 +1,5 @@
 import random, sys
+# A version of this game is featured in the book "Invent Your Own Computer Games with Python"
 
 figuras = [r"""
   +--+
@@ -62,33 +63,43 @@ PALAVRAS = 'MIKE RUI IVO CARRIÇO'.split()
 
 def main():
     print('Enforcado do Nome_do_Ninja')
-
-    letrasFalhadas= []
-    letrasCorretas= []
-    palavraCorreta = random.choice(PALAVRAS)
+    
+    #Variaveis para um novo jogo
+    letrasFalhadas= [] #lista de letras incorretas
+    letrasCorretas= [] #lista de letras corretas
+    palavraCorreta = random.choice(PALAVRAS) # Palavra que o jogador tem que adivinhar
 
     while True: #Loop do jogo principal
         desenharforca(letrasFalhadas, letrasCorretas, palavraCorreta)
+        #Deixar o jogador fazer um palpite
         palpite = palpitedojogador(letrasFalhadas + letrasCorretas)
         
         if palpite in palavraCorreta:
+            #adicionar o palpite às letras corretas
             letrasCorretas.append(palpite)
+            #verificar se o jogador ganhou:
+            venceu = True # Vamos assumir que começa por ganhar o jogador
 
-            venceu = True
             for letrapalavraCorreta in palavraCorreta:
+                # Há uma letra na palavra secreta que não é
+                # ainda em letras corretas, então o jogador não ganhou:
                 if letrapalavraCorreta not in letrasCorretas:
                     venceu = False
                     break
             if venceu:
                 print('Boa a palvra é', palavraCorreta)
                 print('Ganhaste')
+                break # Acaba o jogo (volta ao loop principal)
         else:
+            # O palpite do jogador é errado:
             letrasFalhadas.append(palpite)
-
+            # Verificar se o jogador adivinhou muitas vezes e perdeu.
+            # O "- 1" é porque não contamos a forca vazia em figuras
+    
             if len(letrasFalhadas) == len(figuras) - 1:
                 desenharforca(letrasFalhadas, letrasCorretas, palavraCorreta)
                 print('ficaste sem palpites')
-                print('A palvra era:', palavraCorreta)
+                print('A palvra era "{}"'.format(palavraCorreta))
                 break
 
 
@@ -97,6 +108,7 @@ def desenharforca(letrasFalhadas, letrasCorretas, palavraCorreta):
     print('A categoria é:', CATEGORIA)
     print()
 
+    # Mostra as letras adivinhadas incorretamente:
     print('letrasFalhadas:', end='')
     for letra in letrasFalhadas:
         print(letra, end=' ')
@@ -104,16 +116,19 @@ def desenharforca(letrasFalhadas, letrasCorretas, palavraCorreta):
         print('sem letras falhadas')
     print()
 
+    # Exibe os espaços em branco para a palavra secreta (um espaço em branco por letra):
     espaços = ['_'] * len(palavraCorreta)
 
+    # Substitui os espaços em branco por letras adivinhadas corretamente:
     for i in range(len(palavraCorreta)):
         if palavraCorreta[i] in letrasCorretas:
             espaços[i] = palavraCorreta[i]
 
+    # Mostra a palavra secreta com espaços entre cada letra:
     print(' '.join(espaços))
 
 def palpitedojogador(palpitepreenchido):
-    while True:
+    while True: # Continua a perguntar até que o jogador digite uma letra válida.
         print('Dá um palpite.')
         palpite = input('> ').upper()
         if len(palpite) != 1:
